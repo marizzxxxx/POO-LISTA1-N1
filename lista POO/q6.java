f01f482757e693e2e5f4bbc5be8a2edb397fbb9e
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2023-10-26 13:41:04 -0300

    Create q6.java

---
+++ lista POO/q6.java
@@ -0,0 +1,20 @@
+public class q6 {
+    public static void main(String[] args) {
+        System.out.println("CALCULADORA DE FATORIAIS.\n");
+
+        int x = 10; 
+
+        for (x < 0) {
+            System.out.println("Número inválido. Tente novamente.");
+            System.out.print("Digite um número positivo: ");
+        }
+
+        for (int i = 1; i <= x; i++) {
+            int fat = 1;
+            for (int j = i; j >= 2; j--) {
+                fat *= j;
+            }
+            System.out.printf("%d! = %d%n", i, fat);
+        }
+    }
+}

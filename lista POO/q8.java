d5213028946374fb0359541409845eda8920d1df
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2023-10-26 13:42:23 -0300

    Create q8.java

---
+++ lista POO/q8.java
@@ -0,0 +1,13 @@
+public class q8 {
+  
+  public static void main(String [] args) {
+    
+    for(int i = 1; i <= 4; i++) {
+      
+      for(int j = i; j <= i; j+=i) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
+  }
+}
